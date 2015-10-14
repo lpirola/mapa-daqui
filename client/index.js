a1d7111fabfe92e131dbdac682da8c4f25bf3758
Lucas Pirola <lucaspirola@gmail.com>
2015-10-14 18:43:35 -0300

    Desabilita controle zoom no mapa. closes #14

--- client/index.js
+++ client/index.js
@@ -73,7 +73,7 @@ Template.adotar_mapa.rendered = function () {
 	Deps.autorun(function () {
 		if (Mapbox.loaded()) {
 			L.mapbox.accessToken = 'pk.eyJ1IjoibWFwYWRhcXVpIiwiYSI6IjBiNDkyMjNjOTI2MGYzOGM3YmVlMTdmYjUxZWM3YjNlIn0.wgKsb3mWtdUBhA8CYRWvKQ';
-			map = L.mapbox.map("map", 'mapadaqui.2586fca1', {maxZoom: 17,minZoom: 17}).setView([-23.5462, -46.6514], 17);
+			map = L.mapbox.map("map", 'mapadaqui.2586fca1', {maxZoom: 17,minZoom: 17, zoomControl:false}).setView([-23.5462, -46.6514], 17);
 		}
 	});
 };
