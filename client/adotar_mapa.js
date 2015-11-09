3e925e9d8f90922c73f0073f4cffbe36aa9d303e
Lucas Pirola <lucaspirola@gmail.com>
2015-11-09 16:14:34 -0200

    Divisão do css em componentes
    A partir desse momento a organização do css deve ser orientada a
    metodologia BEM.

--- client/adotar_mapa.js
+++ client/adotar_mapa.js
@@ -1,3 +1,16 @@
+var geocodeAddress = function () {
+	var geocoder = new google.maps.Geocoder();
+	geocoder.geocode({'address': Session.get('address')}, function(results, status) {
+		if (status === google.maps.GeocoderStatus.OK) {
+			var loc = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
+			//L.marker(loc).addTo(map);
+			map.setView(loc)
+		} else {
+			console.log('Geocode was not successful for the following reason: ' + status);
+		}
+	});
+	return false;
+};
 Template.adotar_mapa.rendered = function () {
 	Deps.autorun(function () {
 		if (Mapbox.loaded()) {
--- client/index.js
+++ client/index.js
@@ -9,20 +9,7 @@ Session.setDefault('mensagem_parceiro', '');
 Session.setDefault('instagram', []);
 
 var Signs = new Mongo.Collection("signs"),
-	map,
-	geocodeAddress = function () {
-		var geocoder = new google.maps.Geocoder();
-		geocoder.geocode({'address': Session.get('address')}, function(results, status) {
-			if (status === google.maps.GeocoderStatus.OK) {
-				var loc = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
-				//L.marker(loc).addTo(map);
-				map.setView(loc)
-			} else {
-				console.log('Geocode was not successful for the following reason: ' + status);
-			}
-		});
-		return false;
-	};
+	map;
 
 Meteor.startup(function() {
 	GoogleMaps.load();
