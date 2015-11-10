f16c0ed66dce5f93cbf9cccf3b1da6c99f0b245f
Lucas Pirola <lucaspirola@gmail.com>
2015-11-10 00:07:03 -0200

    Adiciona progresso de importação e download em lote

--- client/admin.js
+++ client/admin.js
@@ -23,25 +23,63 @@ Template.remover_sinalizacao.events({
 	}
 });
 
-Session.setDefault('uploaded_csv', []);
+Session.setDefault('uploaded_data', []);
+Session.setDefault('uploaded_data_size', 0);
 Session.setDefault('prepared_data', []);
+Session.setDefault('saved_data', []);
+Session.setDefault('errored_data', []);
 
 Template.gerar_em_lote.helpers({
 	prepared_data: function () {
 		return Session.get('prepared_data');
+	},
+	errored_data: function () {
+		return Session.get('errored_data');
+	},
+	uploaded_data_size: function () {
+		return Session.get('uploaded_data_size');
+	},
+	prepared_data_size: function () {
+		return _.size(Session.get('prepared_data'));
+	},
+	saved_data_size: function () {
+		return _.size(Session.get('saved_data'));
+	},
+	errored_data_size: function () {
+		return _.size(Session.get('errored_data'));
 	}
 });
 
 Template.gerar_em_lote.events({
+	'click #download_all_prepared': function (evt) {
+		var save_and_download = function () {
+			var prepared = Session.get('prepared_data');
+			var saved = Session.get('saved_data');
+			var v = _.last(prepared);
+			Meteor.call('addSign', v.address, v.lat, v.lng, function (error, newSign) {
+				Meteor.call('generatePdf', newSign, function (error2, result) {
+					download("data:application/pdf;base64," + result, newSign + '.pdf', "application/pdf");
+				});
+			});
+			var new_prepared = _.initial(prepared);
+			Session.set('prepared_data', new_prepared);
+
+			if (_.size(new_prepared) >= 0) {
+				setTimeout(save_and_download, 1000);
+			}
+		};
+		save_and_download();
+		return false;
+	},
 	'submit form': function(evt) {
 		var file = evt.target.arquivo_csv.files[0];
 		var geocoder = new google.maps.Geocoder();
 		var search_cep = function () {
-			var uploaded = Session.get('uploaded_csv');
+			var uploaded = Session.get('uploaded_data');
 			var prepared = Session.get('prepared_data');
 			var v = _.last(uploaded);
 
-			if (parseInt(v.length) > 1) {
+			if ((typeof v !== 'undefined') && (parseInt(v.length) > 1)) {
 				var email = v[0],
 					cep = v[1].replace(/[^0-9]+/g, '');
 
@@ -51,12 +89,16 @@ Template.gerar_em_lote.events({
 						var loc = [results[0].geometry.location.lat(), results[0].geometry.location.lng()]
 						prepared.push({'email':email, 'cep':cep, 'lat':loc[0], 'lng':loc[1], address: address, created_at: new Date});
 						Session.set('prepared_data', prepared);
+					} else {
+						var errored = Session.get('errored_data');
+						errored.push({cep:cep, message: status});
+						Session.set('errored_data', errored);
 					}
 				});
 			}
 
 			var new_csv = _.initial(uploaded);
-			Session.set('uploaded_csv', new_csv);
+			Session.set('uploaded_data', new_csv);
 
 			if (_.size(new_csv) >= 0) {
 				setTimeout(search_cep, 1000);
@@ -64,7 +106,8 @@ Template.gerar_em_lote.events({
 		};
 		Papa.parse(file, {
 			complete: function(results) {
-				Session.set('uploaded_csv', results.data);
+				Session.set('uploaded_data', results.data);
+				Session.set('uploaded_data_size', _.size(results.data));
 				search_cep();
 			}
 		});
